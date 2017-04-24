8158cb72aac260845f3b4bd18cddab908d6cfe62
Diana Catalina Solano Beltrán <dc.solano10@uniandes.edu.co>
2017-04-23 22:57:05 -0500

    Datatable pacientes

--- public/pages/rest/rest.js
+++ public/pages/rest/rest.js
@@ -11,25 +11,13 @@ $(document).ready(function()    {
 
 
                 table.row.add([paciente.cedula, paciente.name, paciente.apellido, paciente.fechaNacimiento, paciente.marcapasosActual, paciente.presionActual, paciente.estresActual, paciente.frecuenciaActual]).draw().nodes().to$().addClass('danger');
-                var allRows = document.getElementsByTagName("tr");
-                console.log(allRows)
 
             } else if (paciente.estado == "AMARILLO") {
 
                 table.row.add([paciente.cedula, paciente.name, paciente.apellido, paciente.fechaNacimiento, paciente.marcapasosActual, paciente.presionActual, paciente.estresActual, paciente.frecuenciaActual]).draw().nodes().to$().addClass('warning');
-                //var allRows = document.getElementsByTagName("tr");
-                //var lastRow = allRows[i];
-                //console.log(i +" "+ paciente.name);
-                //lastRow.setAttribute('class', 'warning');
-                // $('#tablaPacientes tbody').append('<tr class="warning"><td>' + paciente.cedula + '</td><td>' + paciente.name + '</td><td>' + paciente.apellido + '</td><td>' + paciente.edad + '</td><td>' + paciente.marcapasosActual + '</td><td>' + paciente.presionActual + '</td><td>' + paciente.estresActual + '</td><td>' + paciente.frecuenciaActual + '</td></tr>');
-            } else if (paciente.estado == "VERDE") {
-
-
+            }
+            else if (paciente.estado == "VERDE") {
                 table.row.add([paciente.cedula, paciente.name, paciente.apellido, paciente.fechaNacimiento, paciente.marcapasosActual, paciente.presionActual, paciente.estresActual, paciente.frecuenciaActual]).draw().nodes().to$().addClass('success');
-                //var allRows = document.getElementsByTagName("tr");
-                //var lastRow = allRows[i];
-                //console.log(i +" "+ paciente.name);
-                //lastRow.setAttribute('class', 'success');
             }
             table.draw();
         }
