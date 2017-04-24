b073371faf354937ab732013d7eba669d8e9a443
luisplazas <lf.plazas10@uniandes.edu.co>
2017-04-23 19:32:02 -0500

    Removed main navbar trash, extra patient things

--- public/pages/rest/rest.js
+++ public/pages/rest/rest.js
@@ -1,12 +1,9 @@
-$(document).ready(function()
-{
-    var source = "0.0.0.0:9000"
-
-        $.get("http://"+source+"/pacientes",function(data, status)
-        {
-
-            data.forEach(function(paciente) {
-    		$('#tablaPacientes tbody').append('<tr class="success"><td>'+paciente.cedula+'</td><td>'+paciente.name+'</td><td>'+paciente.apellido+'</td><td>'+paciente.edad+'</td><td>'+paciente.marcapasosActual+'</td><td>'+paciente.presionActual+'</td><td>'+paciente.estresActual+'</td><td>'+paciente.frecuenciaActual+'</td></tr>');
-			});
+$(document).ready(function()    {
+    var source = "localhost:9000";
+    $.get("http://"+source+"/pacientes",function(data, status) {
+        console.log(status);
+        data.forEach(function(paciente) {
+        $('#tablaPacientes tbody').append('<tr class="success"><td>'+paciente.cedula+'</td><td>'+paciente.name+'</td><td>'+paciente.apellido+'</td><td>'+paciente.edad+'</td><td>'+paciente.marcapasosActual+'</td><td>'+paciente.presionActual+'</td><td>'+paciente.estresActual+'</td><td>'+paciente.frecuenciaActual+'</td></tr>');
         });
+    });
 });
