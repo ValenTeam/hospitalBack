91aa719eaa6aafc249150a88dcf581b8f05c4b02
Diana Catalina Solano Beltrán <dc.solano10@uniandes.edu.co>
2017-04-24 00:26:34 -0500

    Envío paciente a historia clínica

--- public/pages/rest/rest.js
+++ public/pages/rest/rest.js
@@ -1,14 +1,17 @@
 
 $(document).ready(function()    {
     var source = "localhost:9000";
-    var table = $('#tablaPacientes').DataTable();
+    var listaPacientes;
+    var table = $('#tablaPacientes').DataTable({
+        select:         true
+    });
 
     $.get("http://"+source+"/pacientes",function(data, status) {
         console.log(status);
+        listaPacientes = data;
         for(var i=0; i<data.length; i++) {
             var paciente = data[i];
             if (paciente.estado == "ROJO") {
-
 
                 table.row.add([paciente.cedula, paciente.name, paciente.apellido, paciente.fechaNacimiento, paciente.marcapasosActual, paciente.presionActual, paciente.estresActual, paciente.frecuenciaActual]).draw().nodes().to$().addClass('danger');
 
@@ -22,8 +25,22 @@ $(document).ready(function()    {
             table.draw();
         }
 
+        
+
+        $('#tablaPacientes tbody').on('click', 'tr', function () {
+            var data = table.row( this ).index()
+            //alert( 'You clicked on '+data+'\'s row' + listaPacientes[data].name );
+            var txt = JSON.stringify(listaPacientes[data]);
+            localStorage.setItem("paciente", txt);
+            window.location.href ="/pages/historiaClinica.html";s
+        } );
+
+
+
 
     });
+
+
 
 });
 
